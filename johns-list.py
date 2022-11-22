e01afb6059739bb764bd615a1743b505967edc5b
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2022-11-23 03:15:49 +0700

    init navbar

--- johns-list.py
+++ johns-list.py
@@ -24,6 +24,10 @@ mysql = MySQL(app)
 def index():
     return render_template("index.html")
 
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
 # Main entry point
 if __name__ == '__main__':
     app.run(debug=True)
