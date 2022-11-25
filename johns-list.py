142769606737335ef43c762de1d406161a808e4d
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2022-11-26 04:02:32 +0700

    login in (no functionality)

--- johns-list.py
+++ johns-list.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_mysqldb import MySQL
 import yaml
 
@@ -28,7 +28,21 @@ def index():
 def about():
     return render_template("about.html")
 
+@app.route("/signup/", methods=['GET', 'POST'])
+def signup():
+    if request.method == 'GET':
+        return render_template('signup.html')
+    elif request.method == 'POST':
+        userDetails = request.form
+        p1 = userDetails['email']
+        p2 = userDetails['username']
+        p3 = userDetails['password']
+        return redirect('/')    
+    return render_template('signup.html')
+
 # Main entry point
 if __name__ == '__main__':
     app.run(debug=True)
+
+
 
