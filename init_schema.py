d230727b6c6a861199e897b92d9baad4d8cbfd62
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2022-11-26 20:11:36 +0700

    Connected to db via flask_sqlalchemy, created file for table initialization

---
+++ init_schema.py
@@ -0,0 +1,53 @@
+"""
+This is ONLY responsible for creating tables.
+"""
+
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+import yaml
+
+# Load credentials
+cred = yaml.load(open('cred.yaml'), Loader=yaml.Loader)
+
+# Intialize flask app
+app = Flask(__name__)
+
+# Configs
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://' + cred['mysql_user'] + ':' + cred['mysql_password'] + '@' + cred['mysql_host'] + '/' + cred['mysql_db']
+app.config['SQLALCHEMY_ECHO'] = True # Comment to disable logs
+
+# Connect app's db to SQLAlchemy
+db = SQLAlchemy(app)
+
+#===================================#
+# INITILIAZE TABLES BELOW THIS LINE #
+#===================================#
+
+# User table
+class user(db.Model):
+   user_id = db.Column('user_id', db.Integer, primary_key = True)
+   user_class = db.Column('user_class', db.CHAR(1), primary_key = True)
+   name = db.Column(db.String(20))
+   email = db.Column(db.String(255))
+   username = db.Column(db.String(20))
+   password = db.Column(db.String(255))
+   registered_date = db.Column(db.Date)
+   profile_pic = db.Column(db.LargeBinary)
+  
+
+def __init__(self, email, username, password):
+    self.email = email
+    self.username = username
+    self.password = password
+
+#=============================#
+# END OF TABLE INITIALIZATION #
+#=============================#
+
+# Initialize all tables
+if __name__ == '__main__':  
+    with app.app_context():
+        db.create_all()
+
+
+
--- johns-list.py
+++ johns-list.py
@@ -1,5 +1,16 @@
-from flask import Flask, render_template, request, redirect
-from flask_mysqldb import MySQL
+"""
+Main application file.
+Add and implement entry points here (We could move them out later if desired.)
+"""
+
+# Flask
+from flask import Flask, render_template, request, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
+
+# For hashing
+from werkzeug.security import generate_password_hash, check_password_hash
+
+# Others
 import yaml
 
 # Load credentials
@@ -14,34 +25,58 @@ app.config['MYSQL_USER'] = cred['mysql_user']
 app.config['MYSQL_PASSWORD'] = cred['mysql_password']
 app.config['MYSQL_DB'] = cred['mysql_db']
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-mysql = MySQL(app)
+app.config['SECRET_KEY'] = 'SECRET_KEY' # TODO: REMOVE THIS!
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:johnpass@localhost:13310/johndb'
 
-# Entry points 
-# Note: Make sure entry points are defined above main.
+# Comment to disable logs
+app.config['SQLALCHEMY_ECHO'] = True
+
+# Connect app's db to SQLAlchemy
+db = SQLAlchemy(app)
+
+#=========================================#
+# INITILIAZE ENTRY POINTS BELOW THIS LINE #
+#=========================================#
 
 # Index
 @app.route("/")
 def index():
     return render_template("index.html")
 
+# About page
 @app.route("/about/")
 def about():
     return render_template("about.html")
 
+# Sign up 
 @app.route("/signup/", methods=['GET', 'POST'])
 def signup():
     if request.method == 'GET':
         return render_template('signup.html')
     elif request.method == 'POST':
         userDetails = request.form
+        
+        # Check that passwords matches
+        if userDetails['password'] != userDetails['confirm_password']:
+            flash('Password does not match.', 'danger')
+            return render_template('signup.html')
+        
         p1 = userDetails['email']
         p2 = userDetails['username']
         p3 = userDetails['password']
+        
+        hashed_pw = generate_password_hash(p3)
+        
+        flash('Successfully registered.', 'success')
         return redirect('/')    
     return render_template('signup.html')
 
-# Main entry point
-if __name__ == '__main__':
+#=========================================#
+#    END OF ENTRY POINT INITIALIZATION    #
+#=========================================#
+
+# Main program entry point
+if __name__ == '__main__':  
     app.run(debug=True)
 
 
